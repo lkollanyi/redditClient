6048d1ac03ac2a5604eb134c2da3dc998df884bd
lkollanyi <lkollanyi@gmail.com>
2025-02-13 12:37:42 +0100

    File structure

---
+++ src/components/App Layout/AppLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+
+const AppLayout = () => {
+    return (
+        //layout here
+        //searchbar
+        //sidebar
+        //outlet for main content, error message, etc
+    );
+};
+
+export default AppLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// src/main.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; //store provider
+import { BrowserRouter as Router } from 'react-router-dom';  // Import Router
 import './index.css'
 import App from './App.jsx'
+import store from './store';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
